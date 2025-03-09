ac43a5b717c54132743ccc5dc8da084c6aa7ac67
Lisa Alusa <sayilisaalusa@gmail.com>
2025-03-09 19:36:38 +0300

    continued

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+// Function 5: saturdayFun
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Function 6: mondayWork
+  function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+  // Function 7: wrapAdjective
+  function wrapAdjective(flair = '*') {
+    return function (adjective = 'special') {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
